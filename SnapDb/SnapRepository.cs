4edd402bee44cca2d00ce55f3edf5c708ab3a341
Jesse Barocio <jesse@silverspheredev.com>
2015-03-30 22:06:32 -0600

    Initial commit.

---
+++ SnapDb/SnapRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapDb
+{
+    public class SnapRepository<T>
+    {
+        public IQueryable<T> Get(Expression<Func<T, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
