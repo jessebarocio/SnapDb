9ea6a4a2942f7feab1bb4f2dc77b6198e9a3562f
Jesse Barocio <jesseb@bmisw.com>
2015-03-31 09:31:30 -0600

    Added XML documentation comments to SnapRepository.

--- SnapDb/SnapRepository.cs
+++ SnapDb/SnapRepository.cs
@@ -12,9 +12,17 @@ namespace SnapDb
         private ISnapStore snapStore;
 
 
+        /// <summary>
+        /// Creates a new SnapRepository using the given SnapDb file path. If the file does not exist it will be created.
+        /// </summary>
+        /// <param name="path">The path to the SnapDb file.</param>
         public SnapRepository(string path)
             : this(new FileSnapStore(path)) { }
 
+        /// <summary>
+        /// Creates a new SnapRepository using the given ISnapStore.
+        /// </summary>
+        /// <param name="snapStore">The ISnapStore used to load/save records.</param>
         internal SnapRepository(ISnapStore snapStore)
         {
             this.snapStore = snapStore;
@@ -22,6 +30,9 @@ namespace SnapDb
 
 
         private List<T> records;
+        /// <summary>
+        /// The collection of all records in the SnapDb.
+        /// </summary>
         internal List<T> Records
         {
             get
@@ -31,6 +42,11 @@ namespace SnapDb
         }
 
 
+        /// <summary>
+        /// Gets the records that match the given filter. If no filter is passed all records will be returned.
+        /// </summary>
+        /// <param name="filter">The expression used to filter records.</param>
+        /// <returns>An IQueryable of records matching the filter or all records if filter is null.</returns>
         public IQueryable<T> Get(Expression<Func<T, bool>> filter = null)
         {
             var results = Records.AsQueryable();
@@ -41,16 +57,29 @@ namespace SnapDb
             return results;
         }
 
+        /// <summary>
+        /// Inserts the item into the SnapDb.
+        /// </summary>
+        /// <remarks>You must call SaveChanges() before the record will be saved to the SnapDb store.</remarks>
+        /// <param name="item">The item to insert.</param>
         public void Insert(T item)
         {
             Records.Add(item);
         }
 
+        /// <summary>
+        /// Removes an item from the SnapDb. No exception is thrown if the item is not found in the SnapDb.
+        /// </summary>
+        /// <remarks>You must call SaveChanges() before the record will be removed from the SnapDb store.</remarks>
+        /// <param name="item">The item to remove.</param>
         public void Delete(T item)
         {
             Records.Remove(item);
         }
 
+        /// <summary>
+        /// Saves all changes to the SnapDb store.
+        /// </summary>
         public void SaveChanges()
         {
             snapStore.SaveRecords<T>(Records);
